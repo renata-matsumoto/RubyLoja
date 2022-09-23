9cb874b4befb3abc9d13a7bf1a269912da0a4f74
Renata Matsumoto <renatas.matsumoto@gmail.com>
2022-09-23 11:21:48 -0300

    add new files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
-  root to: "produtos#index"
-
+  get "produtos/busca", to: "produtos#busca", as: :busca_produto
+  resources :produtos, only: [:new, :create, :destroy]
+    root to: "produtos#index"
 end
