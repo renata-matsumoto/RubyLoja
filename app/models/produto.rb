d96dbb320a932d067830cb9fcd23204bb5fd5764
Renata Matsumoto <renatas.matsumoto@gmail.com>
2022-09-25 00:08:33 -0300

    add departamento table

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,2 +1,4 @@
 class Produto < ApplicationRecord
+  validates :preco, presence: true
+  validates :nome, length: {minimum: 4 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_22_013601) do
+ActiveRecord::Schema.define(version: 2022_09_25_025734) do
+
+  create_table "departamentos", force: :cascade do |t|
+    t.string "nome"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "produtos", force: :cascade do |t|
     t.string "nome"
