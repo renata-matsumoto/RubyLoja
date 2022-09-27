7ce8cccdf12791a39949c61b1879be49c786b51d
Renata Matsumoto <renatas.matsumoto@gmail.com>
2022-09-27 16:03:12 -0300

    fix Departaments

--- app/controllers/departamentos_controller.rb
+++ app/controllers/departamentos_controller.rb
@@ -26,7 +26,7 @@ end
 
     respond_to do |format|
       if @departamento.save
-        format.html { redirect_to departamento_url(@departamento), notice: "Departamento was successfully created." }
+        format.html { redirect_to departamentos_url, notice: "Departamento was successfully created." }
         format.json { render :show, status: :created, location: @departamento }
       else
         format.html { render :new, status: :unprocessable_entity }
