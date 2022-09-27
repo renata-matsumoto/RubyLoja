94f923c05acbeef4c585cf1ddaa26fcdee9b6950
Renata Matsumoto <renatas.matsumoto@gmail.com>
2022-09-26 22:50:50 -0300

    add edit component

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,34 +1,66 @@
 class ProdutosController < ApplicationController
 
+  before_action :set_produto, only: [:edit,:update, :destroy]
+
+
   def index
-    @produto = Produto.order(nome: :asc).limit 6
-    @produto_com_desconto = Produto.order(:preco).limit 1    
+      @produto = Produto.order(nome: :asc).limit 6
+      @produto_com_desconto = Produto.order(:preco).limit 1    
   end
   
   def new
-    @produto = Produto.new
-    @departamentos = Departamento.all
+      @produto = Produto.new
+      @departamentos = Departamento.all
   end
 
-  def create
-    valores = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
-    @produto = Produto.new valores
-    if @produto.save
-      flash[:notice] = "Produto salvo com sucesso!"
-    redirect_to root_url
+  def edit
+      renderiza :edit
+  end
+
+  def update
+    if @produto.update produto_params
+        flash[:notice] = "Produto atualizado com sucesso!"
+        redirect_to root_url
     else
-      render :new
+        renderiza :edit
     end
   end
 
+  def create
+      valores = 
+      @produto = Produto.new produto_params
+      if @produto.save
+        flash[:notice] = "Produto salvo com sucesso!"
+      redirect_to root_url
+      else
+        renderiza :new
+      end
+  end
+
   def destroy
-    id = params[:id]
-    Produto.destroy id
-    redirect_to root_url
+      @produto.destroy 
+      redirect_to root_url
   end
 
   def busca
-    @nome = params[:nome]
-    @produtos = Produto.where "nome like ?", "%#{@nome}%"
+      @nome = params[:nome]
+      @produtos = Produto.where "nome like ?", "%#{@nome}%"
   end
+
+  private
+
+  def produto_params
+      params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
+  end
+
+  def set_produto
+      @produto = Produto.find(params[:id])    
+  end
+  
+  def renderiza(view)
+      @departamentos = Departamento.all
+      render view 
+  end
+  
+
 end
